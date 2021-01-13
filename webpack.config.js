241f30487fe21bb2af1a6b6152c5a1d0ecb2ef93
Eric <erictacher@gmail.com>
2021-01-13 11:44:52 -0500

    intial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+//   plugins: [
+
+//     new HtmlWebpackPlugin({
+//         template: './client/index.js'
+//     })
+//     ],
+  devServer: {
+    publicPath: '/build/',
+    // proxy: {
+    //   '/api': 'http://localhost:3000',
+    // },
+  },
+};
